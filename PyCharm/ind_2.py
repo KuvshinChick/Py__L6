1399369858f2da160ab0b93d18b992b9a34eb9d6
KuvshinChick <kuvshinirina75@gmail.com>
2022-11-05 10:15:48 +0300

    corrected ind_2.py

--- PyCharm/ind_2.py
+++ PyCharm/ind_2.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     s = input("Введите предложение: ")
     s_list = list(s)
 
-    for i in range (len(s)):
+    for i in range(len(s)):
         if i % 2 != 0:
             s_list[i] = 'ы'
 
